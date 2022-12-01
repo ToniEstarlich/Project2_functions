5df44b41542bf453dc1a27a8cbd7f73b694b721b
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2022-12-01 23:00:34 +0000

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+function myFuction() {//Definig a function and naming it 
+    var sentence ="I am learning"; //Defining a variable and giving it a
+    sentence += "a lot from this book!"; //Using the senttece
+    document.getElementById("Concatenate").innerHTML = sentence; //Putting the concatenate
+  }
